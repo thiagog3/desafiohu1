4e572dd14185013cb6c2cbef4e7c87ca7d708673
Thiago Sciotta <thiagog3@gmail.com>
2015-08-05 07:52:07 -0300

    Importação dos dados para o lokiJS...

---
+++ utils/csv-loader.js
@@ -0,0 +1,59 @@
+module.exports = (function () {
+  var fs = require('fs');
+
+  function objectLoader(headers, data) {
+    var obj = {},
+      c = 0,
+      len = headers.length;
+    for (c; c < len; c += 1) {
+      obj[headers[c]] = data[c];
+    }
+    return obj;
+  }
+
+  function loadWithHeaders(headers) {
+    return function (data) {
+      return objectLoader(headers, data);
+    };
+  }
+
+  return {
+    loadCsv: function (file, options, callback) {
+      fs.readFile(file, function (err, data) {
+        if (err) {
+          throw err;
+        }
+        var rows = data.toString().split('\n'),
+          i = 0,
+          len = rows.length,
+          opts = options || {},
+          csv = {},
+          localLoader;
+
+        opts.withHeaders = opts.withHeaders || true;
+        opts.intoObjects = opts.intoObjects || true;
+
+        if (opts.withHeaders === true) {
+          csv.headers = rows[0].split(',');
+          i = 1;
+        }
+
+        if (opts.headers){
+          csv.headers = opts.headers;
+        }
+
+        var loader = opts.intoObjects ? loadWithHeaders(csv.headers) : function (data) {
+          return data;
+        };
+
+        for (i; i < len; i += 1) {
+          rows[i] = loader(rows[i].split(','));
+        }
+
+        csv.data = rows;
+
+        callback(csv);
+      });
+    }
+  };
+})();
